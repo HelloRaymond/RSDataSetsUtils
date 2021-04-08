e9829005c837c3ddf976a769c4a5849010612959
Raymond <xurui1995@hotmail.com>
2021-04-08 23:01:57 +0800

    fix bugs

--- sample.py
+++ sample.py
@@ -1,21 +1,21 @@
 from RSDataConvert import RSDataConverter
 
 DOTAConvert = RSDataConverter(
-    dataset_type='DOTA', images_path='TestDataSet/DOTA/train/images', labels_path='TestDataSet/DOTA/train/labelTxt')
+    dataset_type='DOTA', images_folder_path='TestDataSet/DOTA/train/images', labels_folder_path='TestDataSet/DOTA/train/labelTxt')
 DOTAConvert.convert2VOC()
 DOTAConvert.convert2Yolo()
 
-VisDroneConvert = RSDataConverter(dataset_type='VisDrone', images_path='TestDataSet/VisDrone2019/train/images',
-                                  labels_path='TestDataSet/VisDrone2019/train/annotations')
+VisDroneConvert = RSDataConverter(dataset_type='VisDrone', images_folder_path='TestDataSet/VisDrone2019/train/images',
+                                  labels_folder_path='TestDataSet/VisDrone2019/train/annotations')
 VisDroneConvert.convert2VOC()
 VisDroneConvert.convert2Yolo()
 
 VisDroneVOCConvert = RSDataConverter(
-    dataset_type='VOC', images_path='TestDataSet/VisDrone2019_VOC/train/images', labels_path='TestDataSet/VisDrone2019_VOC/train/VOC')
+    dataset_type='VOC', images_folder_path='TestDataSet/VisDrone2019_VOC/train/images', labels_folder_path='TestDataSet/VisDrone2019_VOC/train/VOC')
 VisDroneVOCConvert.convert2Yolo(classes=["ignored regions", "pedestrian", "people", "bicycle",
                                 "car", "van", "truck", "tricycle", "awning-tricycle", "bus", "motor", "others"])
 
-VHRConvert = RSDataConverter(dataset_type='VisDrone', images_path='TestDataSet/NWPU VHR-10/train/positive image set',
-                             labels_path='TestDataSet/NWPU VHR-10/train/ground truth')
+VHRConvert = RSDataConverter(dataset_type='VHR', images_folder_path='TestDataSet/NWPU VHR-10/train/positive image set',
+                             labels_folder_path='TestDataSet/NWPU VHR-10/train/ground truth')
 VHRConvert.convert2VOC()
 VHRConvert.convert2Yolo()
