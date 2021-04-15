085bea36c5f6b0ab63ba9011b80d3da3cb34f0b8
Raymond <xurui1995@hotmail.com>
2021-04-15 11:32:43 +0800

    add plotLabeledImg util

--- RSDataSetsUtils.py
+++ RSDataSetsUtils.py
@@ -15,12 +15,12 @@ The types of data sets currently supported are: PASCAL VOC, VisDrone, DOTA, NWPU
 
 import os
 import cv2
-import time
 from xml.dom import minidom
 import xml.etree.ElementTree as ET
 
 
 class YoloBoudingBox(object):
+
     def __init__(self, center_x, center_y, width, height, cls_id):
         self.center_x = float(center_x)
         self.center_y = float(center_y)
@@ -30,6 +30,7 @@ class YoloBoudingBox(object):
 
 
 class VOCBoudingBox(object):
+
     def __init__(self, x_min, x_max, y_min, y_max, cls_name, is_difficult=False, is_truncated=False):
         self.x_min = int(x_min)
         self.x_max = int(x_max)
@@ -41,48 +42,49 @@ class VOCBoudingBox(object):
 
 
 class VOCLabel(object):
-    def __init__(self, img_path, label_path, clss):
+
+    def __init__(self, img_path, label_path):
         self.img_path = img_path
         self.label_path = label_path
-        self.clss = clss
         self.BBoxes = []
         self.img_w = 0
         self.img_h = 0
         self.img_d = 0
 
-    def getSize(self):
-        img = cv2.imread(img_path)
+    def updateSize(self):
+        img = cv2.imread(self.img_path)
         self.img_w = img.shape[1]
         self.img_h = img.shape[0]
         self.img_d = img.shape[2]
+        return self.img_h, self.img_w, self.img_d
 
-    def getBBoxes(self):
+    def updateBBoxes(self):
         with open(self.label_path, "r") as input_label_file:
             tree = ET.parse(input_label_file)
             root = tree.getroot()
             for obj in root.iter("object"):
                 try:
-                    is_difficult = False if obj.find(
-                        "difficult").text == "0" else True
+                    diffi = False if obj.find("difficult").text == "0" else True
                 except:
-                    is_difficult = False
+                    diffi = False
                 try:
-                    is_truncated = False if obj.find(
-                        "truncated").text == "0" else True
+                    trunc = False if obj.find("truncated").text == "0" else True
                 except:
-                    is_truncated = False
-                cls_name = obj.find("name").text
+                    trunc = False
+                clsname = obj.find("name").text
                 xmlbox = obj.find("bndbox")
-                x_min = float(xmlbox.find("xmin").text)
-                x_max = float(xmlbox.find("xmax").text)
-                y_min = float(xmlbox.find("ymin").text)
-                y_max = float(xmlbox.find("ymax").text)
-                self.BBoxes.append(VOCBoudingBox(x_min, x_max, y_min, y_max, cls_name, is_difficult, is_truncated))
+                x1 = float(xmlbox.find("xmin").text)
+                x2 = float(xmlbox.find("xmax").text)
+                y1 = float(xmlbox.find("ymin").text)
+                y2 = float(xmlbox.find("ymax").text)
+                self.BBoxes.append(VOCBoudingBox(x1, x2, y1, y2, clsname, diffi, trunc))
+        return self.BBoxes
 
-    def Convert2Yolo(self):
+    def convert2YoloLabel(self, clss):
+        self.updateSize()
         dw = 1.0/self.img_w
         dh = 1.0/self.img_h
-        BBoxesYolo = []
+        yolo_label = YoloLabel(self.img_path, self.label_path)
         for box in self.BBoxes:
             x = (box.x_min + box.x_max)/2.0 - 1
             y = (box.y_min + box.y_max)/2.0 - 1
@@ -92,48 +94,47 @@ class VOCLabel(object):
             w = w*dw
             y = y*dh
             h = h*dh
-            BBoxesYolo.append(YoloBoudingBox(x, y, w, h, self.clss.index(box.cls_name)))
-        newYoloLabel = YoloLabel(self.img_path, self.label_path, self.clss)
-        newYoloLabel.BBoxes = BBoxesYolo
-        return newYoloLabel
+            yolo_label.BBoxes.append(YoloBoudingBox(x, y, w, h, clss.index(box.cls_name)))
+        return yolo_label
 
 
 class YoloLabel(VOCLabel):
-    def __init__(self, img_path, label_path, clss):
-        super(YoloLabel, self).__init__(img_path, label_path, clss)
 
-    def getBBoxes(self):
+    def __init__(self, img_path, label_path):
+        super(YoloLabel, self).__init__(img_path, label_path)
+
+    def updateBBoxes(self):
         with open(self.label_path, "r") as input_label_file:
             lines = [input_label_file.readlines()]
             for line in lines:
                 for boxes in line:
                     box = boxes.strip("\n")
                     box = box.split(" ")
-                    cls_name = int(box[0])
-                    x_center = float(box[1])
-                    y_center = float(box[2])
-                    width = float(box[3])
-                    height = float(box[4])
-                    cls_id = self.clss.index(cls_name)
-                    self.BBoxes.append(YoloBoudingBox(x_center, y_center, width, height, cls_id))
+                    clsid = int(box[0])
+                    x = float(box[1])
+                    y = float(box[2])
+                    w = float(box[3])
+                    h = float(box[4])
+                    self.BBoxes.append(YoloBoudingBox(x, y, w, h, clsid))
+        return self.BBoxes
 
-    def Convert2VOC(self):
-        BBoxesVOC = []
+    def convert2VOCLabel(self, clss):
+        self.updateSize()
+        voc_label = VOCLabel(self.img_path, self.label_path)
         for box in self.BBoxes:
-            xmin = int(box.center_x * self.img_w - box.bbox_width * self.img_w / 2)
-            ymin = int(box.center_y * self.img_h - box.bbox_height * self.img_h / 2)
-            xmax = int(box.center_x * self.img_w + ox.bbox_width * self.img_w / 2)
-            ymax = int(box.center_y * self.img_h + box.bbox_height * self.img_h / 2)
-            BBoxesVOC.append(VOCBoudingBox(self.img_w, self.img_h, self.img_w, box.xmin, box.xmax, box.ymin, box.ymax, clss[box.cls_id]))
-        newVOCLabel = VOCLabel(self.img_path, self.label_path, self.clss)
-        newVOCLabel.BBoxes = BBoxesVOC
-        return newVOCLabel
+            x1 = int(box.center_x * self.img_w - box.bbox_width * self.img_w / 2)
+            x2 = int(box.center_x * self.img_w + box.bbox_width * self.img_w / 2)
+            y1 = int(box.center_y * self.img_h - box.bbox_height * self.img_h / 2)
+            y2 = int(box.center_y * self.img_h + box.bbox_height * self.img_h / 2)
+            voc_label.BBoxes.append(VOCBoudingBox(x1, x2, y1, y2, clss[box.cls_id]))
+        return voc_label
 
 
 class RSDataSetsUtils:
 
     @staticmethod
-    def WriteVOCLabelXml(voc_label, xml_path):
+    def writeVOCXmlLabel(voc_label, xml_label_path):
+        voc_label.updateSize()
         img_folder = os.path.split(voc_label.img_path)[0]
         img_name = os.path.split(voc_label.img_path)[1]
         doc = minidom.Document()
@@ -194,11 +195,11 @@ class RSDataSetsUtils:
             bndbox.appendChild(ymax)
             object.appendChild(bndbox)
             annotation.appendChild(object)
-            with open(xml_path, "w") as output_label_file:
+            with open(xml_label_path, "w") as output_label_file:
                 output_label_file.write(doc.toprettyxml())
 
     @staticmethod
-    def WritrYoloLabelTxt(yolo_label, label_txt_path):
+    def writeYoloTxtLabel(yolo_label, label_txt_path):
         with open(label_txt_path, "w") as output_label_file:
             for box in yolo_label.BBoxes:
                 output_label_file.write("%d %.16f %.16f %.16f %.16f\n" % (box.cls_id, box.center_x, box.center_y, box.width, box.height))
@@ -216,7 +217,7 @@ class RSDataSetsUtils:
                     box = boxes.strip("\n")
                     box = box.split(" ")
                     x_min = min(float(box[0]), float(box[2]), float(box[4]), float(box[6]))
-                    y_min = min(float(box[1]), floatbox[3]), float(box[5]), float(box[7]))
+                    y_min = min(float(box[1]), float(box[3]), float(box[5]), float(box[7]))
                     x_max = max(float(box[0]), float(box[2]), float(box[4]), float(box[6]))
                     y_max = max(float(box[1]), float(box[3]), float(box[5]), float(box[7]))
                     cls_name = box[8]
@@ -226,8 +227,8 @@ class RSDataSetsUtils:
 
     @staticmethod
     def getVisDrone(img_path, label_path):
-        classes=["ignored regions", "pedestrian", "people", "bicycle", "car", "van", "truck", "tricycle", "awning-tricycle", "bus", "motor", "others"]
-        voc_label = VOCLabel(img_path, label_path, classes)
+        classes = ["ignored regions", "pedestrian", "people", "bicycle", "car", "van", "truck", "tricycle", "awning-tricycle", "bus", "motor", "others"]
+        voc_label = VOCLabel(img_path, label_path)
         with open(label_path, "r") as input_label_file:
             lines = [input_label_file.readlines()]
             for line in lines:
@@ -246,8 +247,8 @@ class RSDataSetsUtils:
 
     @staticmethod
     def getVHR(img_path, label_path):
-        classes=["airplane", "ship", "storage tank", "baseball diamond", "tennis court", "basketball court", "ground track field", "harbor", "bridge", "vehicle"]
-        voc_label = VOCLabel(img_path, label_path, classes)
+        classes = ["airplane", "ship", "storage tank", "baseball diamond", "tennis court", "basketball court", "ground track field", "harbor", "bridge", "vehicle"]
+        voc_label = VOCLabel(img_path, label_path)
         with open(label_path, "r") as input_label_file:
             lines = [input_label_file.readlines()]
             for line in lines:
@@ -288,6 +289,16 @@ class RSDataSetsUtils:
                 flag = False
         return flag
 
+    @staticmethod
+    def plotLabeledImg(voc_label):
+        img = cv2.imread(voc_label.img_path)
+        for box in voc_label.BBoxes:
+            cv2.putText(img,box.cls_name,(box.x_min, box.y_min), cv2.FONT_HERSHEY_PLAIN, 0.75, (0, 0, 255), thickness=1)
+            cv2.rectangle(img, (box.x_min, box.y_min), (box.x_max, box.y_max), (0, 255, 0), 1)
+        cv2.imshow(voc_label.img_path, img)
+        cv2.waitKey()
+
+
 class RSDataSetsConverter(RSDataSetsUtils):
 
     def __init__(self, dataset_name, dataset_type, images_folder_path, labels_folder_path):
@@ -311,7 +322,7 @@ class RSDataSetsConverter(RSDataSetsUtils):
                 save_path = os.path.join(self.labels_folder_path, "../VOC")
                 if not os.path.exists(save_path):
                     os.makedirs(save_path)
-                self.WriteVOCLabelXml(voc_label, os.path.join(save_path, output_file_name))
+                self.writeVOCXmlLabel(voc_label, os.path.join(save_path, output_file_name))
         elif self.dataset_type == "visdrone":
             for txt in txt_file:
                 txt_full_path = os.path.join(self.labels_folder_path, txt)
@@ -321,7 +332,7 @@ class RSDataSetsConverter(RSDataSetsUtils):
                 save_path = os.path.join(self.labels_folder_path, "../VOC")
                 if not os.path.exists(save_path):
                     os.makedirs(save_path)
-                self.WriteVOCLabelXml(voc_label, os.path.join(save_path, output_file_name))
+                self.writeVOCXmlLabel(voc_label, os.path.join(save_path, output_file_name))
         elif self.dataset_type == "vhr":
             for txt in txt_file:
                 txt_full_path = os.path.join(self.labels_folder_path, txt)
@@ -331,48 +342,50 @@ class RSDataSetsConverter(RSDataSetsUtils):
                 save_path = os.path.join(self.labels_folder_path, "../VOC")
                 if not os.path.exists(save_path):
                     os.makedirs(save_path)
-                self.WriteVOCLabelXml(voc_label, os.path.join(save_path, output_file_name))
+                self.writeVOCXmlLabel(voc_label, os.path.join(save_path, output_file_name))
         else:
             print("type: \"%s\" to voc, Not yet supported!" %(self.dataset_type))
 
-    def convert2Yolo(self, classes=None):
+    def convert2Yolo(self):
         txt_file = os.listdir(self.labels_folder_path)
         txt_file = os.listdir(self.labels_folder_path)
         if self.dataset_type == "dota":
+            classes = ["plane", "ship", "storage-tank", "baseball-diamond", "tennis-court", "basketball-court", "ground-track-field", "harbor", "bridge", "large-vehicle", "small-vehicle", "helicopter", "roundabout", "soccer-ball-field", "swimming-pool", "container-crane", "airport", "helipad"]
             for txt in txt_file:
                 txt_full_path = os.path.join(self.labels_folder_path, txt)
                 img_full_path = os.path.join(self.images_folder_path, os.path.splitext(txt)[0] + ".png")
                 voc_label = self.getDOTA(img_full_path, txt_full_path)
-                yolo_label = voc_label.Convert2Yolo()
+                yolo_label = voc_label.convert2YoloLabel(classes)
                 output_file_name = os.path.basename(img_full_path).split(".")[0] + ".txt"
                 save_path = os.path.join(self.labels_folder_path, "../YOLO")
                 if not os.path.exists(save_path):
                     os.makedirs(save_path)
-                self.WritrYoloLabelTxt(yolo_label, os.path.join(save_path, output_file_name))
+                self.writeYoloTxtLabel(yolo_label, os.path.join(save_path, output_file_name))
         elif self.dataset_type == "visdrone":
+            classes = ["ignored regions", "pedestrian", "people", "bicycle", "car", "van", "truck", "tricycle", "awning-tricycle", "bus", "motor", "others"]
             for txt in txt_file:
                 txt_full_path = os.path.join(self.labels_folder_path, txt)
-                img_full_path = os.path.join(
-                    self.images_folder_path, os.path.splitext(txt)[0] + ".jpg")
+                img_full_path = os.path.join(self.images_folder_path, os.path.splitext(txt)[0] + ".jpg")
                 voc_label = self.getVisDrone(img_full_path, txt_full_path)
-                yolo_label = voc_label.Convert2Yolo()
+                yolo_label = voc_label.convert2YoloLabel(classes)
                 output_file_name = os.path.basename(img_full_path).split(".")[0] + ".txt"
                 save_path = os.path.join(self.labels_folder_path, "../YOLO")
                 if not os.path.exists(save_path):
                     os.makedirs(save_path)
-                self.WritrYoloLabelTxt(yolo_label, os.path.join(save_path, output_file_name))
+                self.writeYoloTxtLabel(yolo_label, os.path.join(save_path, output_file_name))
         elif self.dataset_type == "vhr":
+            classes = ["airplane", "ship", "storage tank", "baseball diamond", "tennis court", "basketball court", "ground track field", "harbor", "bridge", "vehicle"]
             for txt in txt_file:
                 txt_full_path = os.path.join(self.labels_folder_path, txt)
                 img_full_path = os.path.join(
                     self.images_folder_path, os.path.splitext(txt)[0] + ".jpg")
                 voc_label = self.getVHR(img_full_path, txt_full_path)
-                yolo_label = voc_label.Convert2Yolo()
+                yolo_label = voc_label.convert2YoloLabel(classes)
                 output_file_name = os.path.basename(img_full_path).split(".")[0] + ".txt"
                 save_path = os.path.join(self.labels_folder_path, "../YOLO")
                 if not os.path.exists(save_path):
                     os.makedirs(save_path)
-                self.WritrYoloLabelTxt(yolo_label, os.path.join(save_path, output_file_name))
+                self.writeYoloTxtLabel(yolo_label, os.path.join(save_path, output_file_name))
         else:
             print("type: \"%s\" to yolo, Not yet supported!" %(self.dataset_type))
 
@@ -397,13 +410,14 @@ class RSDataSetsConverter(RSDataSetsUtils):
         for i in os.listdir(self.labels_folder_path):
             os.symlink(os.path.join(self.images_folder_path, i), os.path.join(dst_path, self.dataset_name, "label", part, i))
 
+
 class RSDataSetsSpliter(RSDataSetsUtils):
 
     def __init__(self, oringin_imgs_folder_path, oringin_labels_folder_path):
         self.oringin_imgs_folder_path = oringin_imgs_folder_path
         self.oringin_labels_folder_path = oringin_labels_folder_path
 
-    def image_split(self, img_dst_path, size_w, size_h, step):
+    def splitImgs(self, img_dst_path, size_w, size_h, step):
         if not os.path.exists(img_dst_path):
             os.makedirs(img_dst_path)
         img_list = os.listdir(self.oringin_imgs_folder_path)
@@ -460,20 +474,20 @@ class RSDataSetsSpliter(RSDataSetsUtils):
                 name_img = name + "_" + "0" + "_" + "0"
                 cv2.imwrite(os.path.join(img_dst_path, name_img + ".png"), cropped)
 
-    def get_split_voc_label(self, oringin_voc_label, out_img_floder, out_txt_floder, start_x, start_y, size_w, size_h):
+    def getVOCLabelfromOrig(self, oringin_voc_label, out_img_floder, out_label_floder, start_x, start_y, size_w, size_h):
         img_name = os.path.splitext(os.path.split(oringin_voc_label.img_path)[1])
         label_name = os.path.splitext(os.path.split(oringin_voc_label.label_path)[1])
         img_name = os.path.join(out_img_floder, img_name[0] + "_%s_%s" %(start_y, start_x) + img_name[1])
-        label_name = os.path.join(out_txt_floder, label_name[0] + "_%s_%s" %(start_y, start_x) + label_name[1])
-        voc_label = VOCLabel(img_name, label_name, oringin_voc_label.clss)
+        label_name = os.path.join(out_txtout_label_floder_floder, label_name[0] + "_%s_%s" %(start_y, start_x) + label_name[1])
+        voc_label = VOCLabel(img_name, label_name)
         for box in oringin_voc_label.BBoxes:
             if self.__is_between((box.x_min, box.x_max), (start_x, start_x + size_w)) and self.__is_between((box.y_min, box.y_max), (start_y, start_y + size_h)):
                 voc_label.BBoxes.append(VOCBoudingBox(box.x_min - start_x, box.x_max - start_x, box.y_min - start_y, box.y_max - start_y, box.cls_name, box.is_difficult, box.is_truncated))
         return voc_label
 
-    def txt_split(self, img_floder, out_img_floder, txt_floder, out_txt_floder, size_h, size_w):
-        if not os.path.exists(out_txt_floder):
-            os.makedirs(out_txt_floder)
+    def splitLabels(self, img_floder, out_img_floder, txt_floder, out_label_floder, size_h, size_w):
+        if not os.path.exists(out_label_floder):
+            os.makedirs(out_label_floder)
         classes = ["plane", "ship", "storage-tank", "baseball-diamond", "tennis-court", "basketball-court", "ground-track-field", "harbor", "bridge", "large-vehicle", "small-vehicle", "helicopter", "roundabout", "soccer-ball-field", "swimming-pool", "container-crane", "airport", "helipad"]
         img_list = os.listdir(out_img_floder)
         for img_name in img_list:
@@ -483,9 +497,9 @@ class RSDataSetsSpliter(RSDataSetsUtils):
             y = int(name_list[1])
             x = int(name_list[2])
             oringin_voc_label = VOCLabel(os.path.join(img_floder, txt_name + ".png"), os.path.join(txt_floder, txt_name + ".xml"), classes)
-            oringin_voc_label.getBBoxes()
-            voc_label = self.get_split_voc_label(oringin_voc_label, out_img_floder, out_txt_floder, x, y, size_w, size_h)
-            self.WriteVOCLabelXml(voc_label, os.path.join(out_txt_floder, name + ".xml"))
+            oringin_voc_label.updateBBoxes()
+            voc_label = self.getVOCLabelfromOrig(oringin_voc_label, out_img_floder, out_label_floder, x, y, size_w, size_h)
+            self.writeVOCXmlLabel(voc_label, os.path.join(out_label_floder, name + ".xml"))
 
     def delete_empty_sample(self, images_folder_path, labels_folder_path):
         txt_list = os.listdir(images_folder_path)
